0b5f02542d4ea8bcce06a3a3577b632244f9502a
markwst <wongsathorn_w@cmu.ac.th>
2022-03-12 21:04:15 +0700

    update: convert html to ejs

--- app.js
+++ app.js
@@ -5,22 +5,25 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({
-  extended: true
+    extended: true
 }));
 app.use(express.static('public'));
 
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/index.html");
+app.get("/", function(req, res) {
+    res.render('index');
 })
 
-app.get("/indexKitchen.html", function (req, res) {
-  res.sendFile(__dirname + "/indexKitchen.html");
-})
 
-app.get("/indexCustomer.html", function (req, res) {
-  res.sendFile(__dirname + "/indexCustomer.html");
-})
 
+app.get("/indexCustomer.ejs", function(req, res) {
+    res.render('indexCustomer');
+})
+app.get("/indexKitchen.ejs", function(req, res) {
+    res.render('indexKitchen');
+})
+app.get("/register.ejs", function(req, res) {
+    res.render('register');
+})
 app.listen("3000", () => {
     console.log("Server is running on Port 3000.");
-  });
+});
